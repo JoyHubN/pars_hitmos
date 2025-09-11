8f23910fdb6cf2dd3fe2af2f08886a73d3640056
dev <n.cowalyow2017@yandex.ry>
2025-09-11 11:52:19 +0300

    изменен автор

--- parse_hitmos/__init__.py
+++ parse_hitmos/__init__.py
@@ -2,5 +2,5 @@ from .entered_tracks import EnteredTrack
 from .rating_tracks_count import RatingCount
 from .rating_tracks_page import RatingPage
 
-__author__ = 'Joy_079'
+__author__ = 'JoyHubN'
 __version__= '1.0.0'
